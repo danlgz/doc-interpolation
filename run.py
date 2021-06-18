c4af51ca511efae63f5baa10c26ca2505acf9131
danlgz <danielalejandro137@gmail.com>
2021-06-18 11:52:51 -0600

    Fix remove comments

--- run.py
+++ run.py
@@ -1,21 +1,10 @@
 from docxtpl import DocxTemplate
 from datetime import datetime
 import subprocess
-from uuid import uuid4
+from time import time
 
-# from comtypes import client
 
-# def convert_to_pdf(doc):
-#     try:
-#         word = client.DispatchEx("Word.Application")
-#         new_name = doc.replace(".docx", r".pdf")
-#         worddoc = word.Documents.Open(doc)
-#         worddoc.SaveAs(new_name, FileFormat = 17)
-#         worddoc.Close()
-#     except Exception as e:
-#         return e
-#     finally:
-#         word.Quit()
+starts = time()
 
 DATA_SET = {
     'user': {
@@ -40,17 +29,11 @@ DATA_SET = {
 doc = DocxTemplate('./docs/contract.docx')
 doc.render(DATA_SET)
 
-destiny = './docs/output.docx'.format(uuid4())
+destiny = './docs/output.docx'
 doc.save(destiny)
 
 p = subprocess.Popen(['libreoffice', '--convert-to', 'pdf', destiny, '--outdir', './docs'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 outs, errs = p.communicate()
 
-# with open('./docs/output-{}.pdf'.format(uuid4()), 'wb') as f:
-#     f.write(outs)
-#
-# print(type(outs), 'error ->', errs)
-
-
-# word = client.CreateObject('Word.Application')
-# word.Do
+delta = round((time() - starts) * 1000, 2)
+print('Time: {}ms'.format(delta))
